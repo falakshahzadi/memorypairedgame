59c69dfd5c645306f4d73522514164ea2004d0ae
falakshahzadi <98768215+falakshahzadi@users.noreply.github.com>
2023-09-04 18:00:52 +0500

    memorypaired

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import { Box } from "@mui/material";
+import Maincopy from './Components/Maincopy';
+import Maincard from './Components/Maincard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Box py={'120px'}  sx={{backgroundColor:"#96E2F9"}}>
+     <Maincard/>
+     </Box>
+    </>
   );
 }
 
